5a39241887d1ea94baf71614f13acf61981631ff
zandani <41866207+zandani@users.noreply.github.com>
2020-02-28 14:40:41 -0500

    Add files via upload

--- project 7/js/app.js
+++ project 7/js/app.js
@@ -109,3 +109,22 @@ let mobileChart = new Chart(mobileCanvas, {
     },
   }
 });
+
+// Message section
+
+const user = document .getElementById( "userField" );
+const message = document .getElementById( "messageField" );
+const send = document .getElementById( "send" );
+
+send.addEventListener( 'click' , () => {
+// ensure user and message fields are filled out
+if (user.value === "" && message.value === "" ) {
+alert( "Please fill out user and message fields before sending" );
+} else if (user.value === "" ) {
+alert( "Please fill out user field before sending" );
+} else if (message.value === "" ) {
+alert( "Please fill out message field before sending" );
+} else {
+alert( `Message successfully sent to: ${user.value}` );
+}
+});
